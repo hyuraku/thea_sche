9cb454ac0451424331334f7c2273dab73a5851e7
hyuraku <yu@hill-wood.com>
2018-07-01 22:36:35 +0900

    bootstrap,axiosをwebpackで管理

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue =  require('./loaders/vue')
+const webpack = require('webpack')
 
 environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
+environment.plugins.prepend( // Webpacker 3.3.0 以降は set ではなく prepend を使用する
+  'Provide',
+  new webpack.ProvidePlugin({
+    jQuery: 'jquery/dist/jquery',
+    Popper: 'popper.js/dist/popper'
+  })
+)
 environment.loaders.append('vue', vue)
 module.exports = environment
