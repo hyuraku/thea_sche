d506755a105930d125907cc4466b2c1fb0bef1d2
hyuraku <yu@hill-wood.com>
2019-04-02 23:09:48 +0900

    fix n+1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,11 +56,11 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.after_initialize do
-    Bullet.enable  = true   # bullet を有効にする
-
-    Bullet.alert   = true
-    Bullet.bullet_logger = true
-    Bullet.rails_logger = true
-    Bullet.add_footer   = true
+    # Bullet.enable  = true   # bullet を有効にする
+    #
+    # Bullet.alert   = true
+    # Bullet.bullet_logger = true
+    # Bullet.rails_logger = true
+    # Bullet.add_footer   = true
   end
 end
